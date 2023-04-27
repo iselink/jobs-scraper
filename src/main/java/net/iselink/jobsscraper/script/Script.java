6761cb269adfaa45d0391f8bbd8676f4a700ea4d
iselink <33873947+iselink@users.noreply.github.com>
2023-04-27 18:35:41 +0200

    implemented JS engine

--- src/main/java/net/iselink/jobsscraper/script/Script.java
+++ src/main/java/net/iselink/jobsscraper/script/Script.java
@@ -1,8 +1,6 @@
 package net.iselink.jobsscraper.script;
 
-import net.iselink.jobsscraper.Scraper;
-import net.iselink.jobsscraper.utils.Configuration;
-import net.iselink.jobsscraper.utils.DiscordWebHook;
+import net.iselink.jobsscraper.script.classes.ProgramJs;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ScriptableObject;
 
@@ -14,40 +12,34 @@ import java.nio.file.Files;
 
 public class Script {
 
-	private String source = "<?>";
-	private String content = "";
+    private String source = "<?>";
+    private String content = "";
 
-	public Script(String source, String content) {
-		this.source = source;
-		this.content = content;
-	}
+    public Script(String source, String content) {
+        this.source = source;
+        this.content = content;
+    }
 
-	public static Script loadFromFile(String filename) throws IOException {
-		String content = Files.readString(new File(filename).toPath(), StandardCharsets.UTF_8);
-		return new Script(filename, content);
-	}
+    public static Script loadFromFile(String filename) throws IOException {
+        String content = Files.readString(new File(filename).toPath(), StandardCharsets.UTF_8);
+        return new Script(filename, content);
+    }
 
-	public void execute() {
-		Context cx = Context.enter();
-		try {
-			cx.setLanguageVersion(Context.VERSION_ES6);
-			ScriptableObject scope = cx.initSafeStandardObjects();
+    public void execute() {
+        try (Context cx = Context.enter()) {
+            cx.setLanguageVersion(Context.VERSION_ES6);
+            ScriptableObject scope = cx.initStandardObjects();
 
-//			Object webHook = Context.javaToJS(DiscordWebHook.class, scope);
-//			ScriptableObject.putProperty(scope, "WebHook", webHook);
-//
-//			Object configuration = Context.javaToJS(Configuration.class, scope);
-//			ScriptableObject.putProperty(scope, "Configuration", configuration);
-//
-//			Object scraper = Context.javaToJS(Scraper.class, scope);
-//			ScriptableObject.putProperty(scope, "Scraper", scraper);
+            ScriptableObject.putProperty(scope, "out", Context.javaToJS(System.out, scope));
+            ScriptableObject.putProperty(scope, "err", Context.javaToJS(System.err, scope));
 
-			Object out = Context.javaToJS(System.out, scope);
-			ScriptableObject.putProperty(scope, "out", out);
+            ScriptableObject.defineClass(scope, ProgramJs.class);
 
-			cx.evaluateString(scope, content, source, 1, null);
-		} finally {
-			cx.close();
-		}
-	}
+            cx.evaluateString(scope, content, source, 1, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ src/main/java/net/iselink/jobsscraper/script/classes/ProgramJs.java
@@ -0,0 +1,56 @@
+package net.iselink.jobsscraper.script.classes;
+
+import net.iselink.jobsscraper.Scraper;
+import net.iselink.jobsscraper.utils.Configuration;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.annotations.JSStaticFunction;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Class implementing program-wide functions and general helpful functions which makes easy to write scripts.
+ * Also, useful if rhino has been initiated with safe objects (i.e. unable to use Packets like this: new Packages.net.iselink.jobsscraper.Scraper('nachod', [''], 'high', 40);)
+ */
+public class ProgramJs extends ScriptableObject {
+
+    @JSStaticFunction
+    public static Configuration loadConfig(Context cx, Scriptable thisObj, Object[] args, Function funObj) throws IOException {
+        if (args.length != 1) {
+            Context.reportError("Invalid arg count.");
+            return null;
+        }
+
+        return Configuration.loadConfig((String) args[0]);
+    }
+
+    @JSStaticFunction
+    public static Scraper newScraper(Context cx, Scriptable thisObj, Object[] args, Function funObj) throws IOException {
+        if (args.length != 4) {
+            Context.reportError("Invalid arg count.");
+            return null;
+        }
+        return new Scraper(
+                (String) Context.jsToJava(args[0], String.class),
+                (String[]) Context.jsToJava(args[1], String[].class),
+                (String) Context.jsToJava(args[2], String.class),
+                (Integer) Context.jsToJava(args[3], Integer.class));
+    }
+
+    @JSStaticFunction
+    public static void print(Context cx, Scriptable thisObj, Object[] args, Function funObj) {
+        String builder = Arrays
+                .stream(args)
+                .map(o -> Context.jsToJava(o, Object.class).toString()).collect(Collectors.joining());
+        System.out.println(builder);
+    }
+
+    @Override
+    public String getClassName() {
+        return "Program";
+    }
+}
